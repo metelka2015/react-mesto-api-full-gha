e3fb09a506a69f70f1c85f195aaf75996bac3346
Elena <metelka2015@gmail.com>
2023-09-25 13:17:21 +0500

    fix: тест

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { regExp } = require('./utils/constants');
 
-const { PORT, DB_URL } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 mongoose
   .connect(DB_URL, {
