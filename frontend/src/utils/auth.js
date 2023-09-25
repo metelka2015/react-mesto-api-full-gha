e52953321c58d2cfbb341e21138e88fed6463841
Elena <metelka2015@gmail.com>
2023-09-25 18:22:07 +0500

    fix: тест

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -7,40 +7,34 @@ function checkRes(res) {
   return Promise.reject(`Ошибка: ${res.status}`);
 }
 
-//универсальная функция запроса с проверкой ответа
-function request(url, options) {
-  // принимает два аргумента: урл и объект опций, как и `fetch`
-  return fetch(url, options).then(checkRes)
-}
-
 export const register = (email, password) => {
-  return request(`${BASE_URL}/signup`, {
+  return fetch(`${BASE_URL}/signup`, {
     method: "POST",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ email, password }),
-  });
+  }).then(checkRes);
 }
 
 export const login = (email, password) => {
-  return request(`${BASE_URL}/signin`, {
+  return fetch(`${BASE_URL}/signin`, {
     method: "POST",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ email, password }),
-  });
+  }).then(checkRes);
 };
 
 export const checkToken = (token) => {
-  return request(`${BASE_URL}/users/me`, {
+  return fetch(`${BASE_URL}/users/me`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     },
-  });
+  }).then(checkRes);
 }
