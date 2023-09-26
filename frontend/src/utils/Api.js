d7a540d118b42260a997c108d0a4fdaf186dabd0
Elena <metelka2015@gmail.com>
2023-09-27 00:38:08 +0500

    test test

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -119,7 +119,7 @@ export default class Api {
 }
 
 export const api = new Api({
-  url: "https://api.metelka.nomoredomainsrocks.ru",
+  url: "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
   },
--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://api.metelka.nomoredomainsrocks.ru";
+export const BASE_URL = "http://localhost:3000";
 
 function checkRes(res) {
   if (res.ok) {
