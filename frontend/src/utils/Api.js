be4a5abb2a12a78e2f1204f515e9f80171f6f242
Elena <metelka2015@gmail.com>
2023-09-26 14:17:49 +0500

    new test

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -119,7 +119,7 @@ export default class Api {
 }
 
 export const api = new Api({
-  url: "https://api.metelka.nomoredomainsrocks.ru",
+  url: "https://metelka.nomoredomainsrocks.ru",
   headers: {
     "Content-Type": "application/json",
   },
